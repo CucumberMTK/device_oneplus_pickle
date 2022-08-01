237b0713a1e1c36475fa9294cf718715d92caf2c
wbs306 <wbs306@gmail.com>
2022-08-01 22:00:02 +0800

    pickle: Build missing USB hidl

--- device.mk
+++ device.mk
@@ -219,6 +219,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.oplus
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3.vendor \
+    android.hardware.usb.gadget@1.1.vendor
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
