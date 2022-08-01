376b3bc1bf7a66451ada29f2e0998fb54053d97d
wbs306 <wbs306@gmail.com>
2022-08-01 22:00:02 +0800

    pickle: Build missing USB hidl

--- device.mk
+++ device.mk
@@ -194,6 +194,11 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3.vendor \
+    android.hardware.usb.gadget@1.1.vendor
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
